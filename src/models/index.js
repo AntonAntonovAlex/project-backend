1b1cb18ee089d2ea8aab25817a79f989b9625631
Anton Antonau <anton.antonau.mogilev@gmail.com>
2024-12-03 21:11:03 +0300

    change index

--- src/models/index.js
+++ src/models/index.js
@@ -15,7 +15,7 @@ let sequelize;
 //  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 //} else {
 
-if (process.env.DATABASE_URL) {
+if (env === 'production' && process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
